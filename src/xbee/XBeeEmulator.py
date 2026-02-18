ef522958aab6dd22cfc4d9091293b8d971010e8f
jhhocs <joshuaho1@cpp.edu>
2026-02-17 23:56:52 -0800

    feat: add constructor for XBeeEmulator

--- src/xbee/XBeeEmulator.py
+++ src/xbee/XBeeEmulator.py
@@ -1,12 +1,13 @@
 import queue
-from serial_io import ISerial
 from xbee import XBee
 from xbee.frames import x81, x88, x89 # Frame parser for classes for each Xbee frame type
 from logger import Logger    # Custom logging class
+import paho.mqtt.client as mqtt
 
-class XBeeEmulator(ISerial):
+class XBeeEmulator(XBee):
 
-    def __init__(self, port: str = None, baudrate: int = 115200, status: bool = False, logger: Logger = None, config_file: str = None):
+    def __init__(self, *args, logger: Logger = None, mqtt_client: mqtt.Client = None, **kwargs):
+    # def __init__(self):
         """Initialize serial connection
 
         Args:
@@ -15,35 +16,18 @@ class XBeeEmulator(ISerial):
           status: Automatically receive status packets after a transmission.
           logger: Logger instance
         """
-        self.port = port    # Serial port to use
-        self.baudrate = baudrate     # Communication speed  
-        self.ser = None      # Will hold the actual Serial object
-        self.status = status     # If True, it will try to read back status frames (0x89)
-        
+        print("!! RUNNING XBeeEmulator !!")
+
+        self.logger = logger or Logger()
+
         if logger is None:  
-            self.logger = Logger()   # Create logger if not provided
             self.logger.write("LOGGER CREATED By XBeeEmulator.py")
         else:
             self.logger.write("XBeeEmulator")
-            self.logger = logger
-        self.timeout = 0.1 # Allow programmer to configure timeout? # Max time to wait for responses
-        self.status_timeout = 0.2
-        self.frame_id = 0x01    # Frame ID (used to track commands)
-
-        self.config_file = config_file # Add AT_Config.py file  # Path to config file with AT commands 
-
-        # Retrieve Queues
-        self.x81_queue: queue.Queue = queue.Queue()
-        self.x88_queue: queue.Queue = queue.Queue() # If working properly, this queue should never have more than 1 element
-        self.x89_queue: queue.Queue = queue.Queue()
-
-        # Transmit Queue
-        self.transmit_queue: queue.Queue = queue.Queue()
-
-        # self.__transmitting = False # Flag: are we currently sending?
-        # self.__receiving = False    # Flag: are we currently receiving?d1b2fd40841964d904a7927082
-
-        self.logger.write(f"port: {self.port}, baudrate: {self.baudrate}, timeout: {self.timeout}, config_file: {self.config_file}")    # Log configuration for debugging
+            
+        super().__init__(*args, logger=self.logger, **kwargs)
+        self.mqtt_client = mqtt_client
+        
 
     def open(self) -> bool:
         """Opens serial port.
